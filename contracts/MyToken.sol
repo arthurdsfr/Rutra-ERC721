09e7e5a0b0528044e6f8ce211e57339e001eca03
arthurdsfr <arthur.desfrancais@gmail.com>
2024-09-14 17:15:46 +0200

    Constructor + Ownable

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721 {
-    constructor() ERC721("MyToken", "MTK") {}
+contract Rutra_Collection_NFT is ERC721, Ownable {
+    constructor() ERC721("RutraNFT", "RUTRA") Ownable(msg.sender) {
+
+    }
+    
 }
