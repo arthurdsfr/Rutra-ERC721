f83c158f0511c852e883cd77bbe9e4a0b2eb35fd
arthurdsfr <arthur.desfrancais@gmail.com>
2024-09-15 23:26:59 +0200

    Comment + mint

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,8 +6,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Rutra_Collection_NFT is ERC721, Ownable {
-    constructor() ERC721("RutraNFT", "RUTRA") Ownable(msg.sender) {
 
+    // ==== 1. Property Variables =====//
+
+        using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    // ==== 2. Lifecycle methods =====//
+
+    constructor() ERC721("RutraNFT", "RUTRA") Ownable(msg.sender) {
     }
     
+    // ==== 3. Mint/Burn Functions ====//
+
+     function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
 }
