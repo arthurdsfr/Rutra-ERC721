0938efb45c6b6d3645984b29dc02f72ed997b7d0
arthurdsfr <arthur.desfrancais@gmail.com>
2024-09-16 18:56:58 +0200

    from ERC721 to ERC721URI and function safeMint

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Rutra_Collection_NFT is ERC721, Ownable {
+contract Rutra_Collection_NFT is ERC721URIStorage, Ownable {
 
     // ==== 1. Property Variables =====//
 
         using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
-
     // ==== 2. Lifecycle methods =====//
 
     constructor() ERC721("RutraNFT", "RUTRA") Ownable(msg.sender) {
@@ -19,10 +19,11 @@ contract Rutra_Collection_NFT is ERC721, Ownable {
     
     // ==== 3. Mint/Burn Functions ====//
 
-     function safeMint(address to) public onlyOwner {
+     function safeMint(address _to, string memory tokenURI) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+        _safeMint(_to, tokenId); // more securized than _mint
+        _setTokenURI(tokenId, tokenURI); //
     }
 
 }
